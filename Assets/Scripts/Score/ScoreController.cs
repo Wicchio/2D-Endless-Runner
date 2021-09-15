4f663bb52eae748f44bc9686997646be0d930b70
Wicchio <wisnubayu00@gmail.com>
2021-09-15 16:20:30 +0700

    2DRunner

---
+++ Assets/Scripts/Score/ScoreController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+    [Header("Score Highlight")]
+    public int scoreHighlightRange;
+    public CharacterSoundController sound;
+    [Header("Speed Up")]
+    public CharacterMoveController player;
+    public float speedAdditioner;
+
+    private int currentScore = 0;
+    private int lastScoreHighlight = 0;
+
+
+
+    private void Start()
+    {
+        // reset
+        currentScore = 0;
+        lastScoreHighlight = 0;
+    }
+
+    public float GetCurrentScore()
+    {
+        return currentScore;
+    }
+
+    public void IncreaseCurrentScore(int increment)
+    {
+        currentScore += increment;
+
+        if (currentScore - lastScoreHighlight > scoreHighlightRange)
+        {
+            sound.PlayScoreHighlight();
+            player.IncreaseGameSpeed(speedAdditioner);
+            lastScoreHighlight += scoreHighlightRange;
+        }
+    }
+
+    public void FinishScoring()
+    {
+        // set high score
+        if (currentScore > ScoreData.highScore)
+        {
+            ScoreData.highScore = currentScore;
+        }
+    }
+}
